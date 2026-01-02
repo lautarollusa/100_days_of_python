1b108f2ce3225e155d2c84b8a94fec3a39a02aa8
Lautaro <llusa.lautaro@gmail.com>
2026-01-01 23:16:44 -0300

    Initial commit - Day 01

---
+++ day_01/task.py
@@ -0,0 +1,29 @@
+#1 PRINTING
+print("Hello World!")
+
+#2 STRING MANIPULATION
+# Write your code below this line 👇
+print("Hello world!\nHello world!\nHello world!")
+
+print("Hello" + " " + "Angela")
+
+#3 INPUTS
+print("Hello" + " " + input("What is your name?") + "!")
+
+#4 VARIABLES
+print(len(input("What is your name?")))
+
+username = input("What is your name?")
+length = len(username)
+print(length)
+
+#5 VARIABLE NAMING
+name = "Angela"
+length = len(name)
+print(length)
+
+#BAND NAME GENERATOR PROJECT
+print("Welcome to the Band Name Generator!")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's the name of your pet\n")
+print("Your band name could be: " + city + " " + pet)
